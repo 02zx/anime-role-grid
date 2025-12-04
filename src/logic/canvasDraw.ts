510b0995372c788b5cd23cc9cd39db3e935ed843
02zx <114906397+02zx@users.noreply.github.com>
2025-12-04 21:10:11 +0800

    Update canvasDraw.ts

--- src/logic/canvasDraw.ts
+++ src/logic/canvasDraw.ts
@@ -47,7 +47,8 @@ export class CanvasGenerator {
         // Add cache bust to prevent caching of corrupted images
         // Add n=-1 to disable optimization which might cause issues for some images
        // return `https://wsrv.nl/?url=${encodeURIComponent(url)}&output=png&n=-1&t=${Date.now()}`
-        return `https://image.uisdc.cn/?url=${encodeURIComponent(url)}`
+       // return `https://image.uisdc.cn/?url=${encodeURIComponent(url)}`
+        return `https://cors.bridged.cc/${url}`
     }
 
     private loadImage(url: string): Promise<HTMLImageElement> {
